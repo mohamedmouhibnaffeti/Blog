702e7db72673c088c161910e7534af8a7e876f76
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-05 07:07:32 +0100

    Fixed issue

--- blog/blogApp/views.py
+++ blog/blogApp/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Comment
 
-def post_list(request):
+def posts_list(request):
     posts = Post.objects.filter(status='published').order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': post})
 
-def post
+def post_details(request):
+    
