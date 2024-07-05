25013551b9006ec2269670abdfd3b22727fa600a
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-05 07:11:40 +0100

    Fixed issue

--- blog/blogApp/views.py
+++ blog/blogApp/views.py
@@ -4,9 +4,9 @@ from .models import Post, Comment
 
 def posts_list(request):
     posts = Post.objects.filter(status='published').order_by('-published_date')
-    return render(request, 'blogApp/post_list.html', {'posts': posts})
+    return render(request, 'post_list.html', {'posts': posts})
 
 def post_details(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blogApp/post_details.html', {'post': post})
+    return render(request, 'post_details.html', {'post': post})
 
