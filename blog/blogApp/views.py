9c021e9f6eeab6dabfb92b8e8253e806b95f69af
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-05 07:06:19 +0100

    Fixed issue

--- blog/blogApp/views.py
+++ blog/blogApp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 
 def post_list(request):
-    post = Post.objects.filter()
+    post = Post.objects.filter(status='published')
