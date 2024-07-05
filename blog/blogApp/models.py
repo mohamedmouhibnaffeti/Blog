66bbcd19fa288621c91811529aa533192b47359b
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-05 07:04:10 +0100

    Fixed issue

--- blog/blogApp/models.py
+++ blog/blogApp/models.py
@@ -18,4 +18,4 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     content = models.TextField()
-    
+    created_at = 
