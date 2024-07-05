26c0116d76f2bf4a99fca6b7a8be24ebb5aac353
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-05 07:14:35 +0100

    Fixed issue

--- blog/blogApp/urls.py
+++ blog/blogApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.posts_list, name='posts_list'),
+    path('/posts', views.posts_list, name='posts_list'),
     path('/post/<int:pk>/', views.post_details, name='post_details')
 ]
