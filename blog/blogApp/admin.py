883d997adbd1f5991e867134934ced2050511d9b
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-05 07:22:45 +0100

    Fixed issue

--- blog/blogApp/admin.py
+++ blog/blogApp/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'status')
+    list_filter = ('status', 'published_date')
+    search_fields = ('title', 'content', 'author')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('author', 'content')
+
+# Register your models with custom admin classes
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
