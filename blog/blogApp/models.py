b1db36a533bdeb8e2764cd75ad6f11c4b299ab94
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-05 07:02:12 +0100

    Fixed issue

--- blog/blogApp/models.py
+++ blog/blogApp/models.py
@@ -7,4 +7,5 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.CharField(max_length=200)
-    published_date = models.DateField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    
