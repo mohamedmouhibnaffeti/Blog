9108614bbf4c3c7b70b12d3dcb912ae83ab66179
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-05 07:03:40 +0100

    Fixed issue

--- blog/blogApp/models.py
+++ blog/blogApp/models.py
@@ -15,4 +15,5 @@ class Post(models.Model):
     
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    
